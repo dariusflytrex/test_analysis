3449d30a99709dc15ff3ae46ddc0556df984371c
Darius <darius@flytrex.com>
2016-10-31 12:14:29 +0200

    new_data_time_now

--- time.py
+++ time.py
@@ -1,5 +1,12 @@
-import os
+import subprocess
 import time
+
+
+package = "Package."
+users = "Users."
+dashboard = "Dashboard."
+site = "AddSite."
+routes = "Routes."
 
 
 def find_time(target, data=""):
@@ -11,28 +18,44 @@ def find_time(target, data=""):
 
 
 def find_total_time():
-    return find_time("Package.") + find_time("Dashboard.") + find_time("Users.") + find_time("AddSite.") + find_time(
-        "Routes")
+    return find_time(package) \
+           + find_time(dashboard) \
+           + find_time(users) \
+           + find_time(site) \
+           + find_time(routes)
 
 
 def push_to_git():
-    time_now = time.strftime("%d/%b/%H/%M")
+    time_now = time.strftime("%d/%b_%H:%M")
     print time_now
-    os.popen("git add.")
+    subprocess.call("git add .")
     print "added"
-    os.popen('git commit -m "new_data_"' + 'time_now')
+    subprocess.call('git commit -m "new_data_"' + 'time_now')
     print "committed"
-    os.popen("git push origin master")
+    subprocess.call("git push origin master")
     print "updated"
 
-data = find_total_time()
-count = data.count("\n")
+
+def strip_data(test_type, data):
+    data = data.split(test_type, 1)[1]
+    f = data[0:data.index("\n")]
+    file_new.write(f + "\n")
+    return data
 
 with open("data.txt", 'a') as file_new:
-    while count > 1:
-        data = data.split("Package.", 1)[1]
-        f = data[0:data.index("\n")]
-        file_new.write(f + "\n")
+    data = find_total_time()
+    count = data.count("\n")
+    while count > 0:
+        if package in data:
+            data = strip_data(package, data)
+        elif users in data:
+            data = strip_data(users, data)
+        elif dashboard in data:
+            data = strip_data(dashboard, data)
+        elif site in data:
+            data = strip_data(site, data)
+        elif routes in data:
+            data = strip_data(routes, data)
         count -= 1
 
 push_to_git()
