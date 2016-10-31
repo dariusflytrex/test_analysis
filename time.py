5c857e120664554a3fa914fa5904ba7de2db5f85
Darius <darius@flytrex.com>
2016-10-31 12:41:41 +0200

    new_data_31Oct12h41m

--- time.py
+++ time.py
@@ -33,11 +33,11 @@ def strip_data(test_type, data):
 
 
 def push_to_git():
-    time_now = time.strftime("%dd%bm%Hh%Mm")
+    time_now = time.strftime("%d%b%Hh%Mm")
     subprocess.call("git add .")
     subprocess.call('git commit -m "new_data_"' + time_now)
     subprocess.call("git push origin master")
-    subprocess.call("mv new.txt teston%s.txt" %time_now)
+    subprocess.call("mv new.txt test_on_%s.txt" %time_now)
 
 
 with open("data.txt", 'a') as file_new:
