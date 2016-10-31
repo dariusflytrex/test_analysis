c4761f27fdf3766fb3f9b0c48a417790b9c490d7
Darius <darius@flytrex.com>
2016-10-31 12:27:35 +0200

    new_data_31/Oct_12:27

--- time.py
+++ time.py
@@ -9,40 +9,39 @@ site = "AddSite."
 routes = "Routes."
 
 
-def find_time(target, data=""):
-    with open("test.txt", 'r') as file:
+def filter_data(target, data=""):
+    with open("new.txt", 'r') as file:
         for line in file:
             if target in line:
                 data += str(line)
     return data.replace(" ", "")
 
 
-def find_total_time():
-    return find_time(package) \
-           + find_time(dashboard) \
-           + find_time(users) \
-           + find_time(site) \
-           + find_time(routes)
+def compile_data():
+    return filter_data(package) \
+           + filter_data(dashboard) \
+           + filter_data(users) \
+           + filter_data(site) \
+           + filter_data(routes)
+
+
+def strip_data(test_type, data):
+    info = data.split(test_type, 1)[1]
+    i = info[0:info.index("\n")]
+    file_new.write(i + "\n")
+    return info
 
 
 def push_to_git():
     time_now = time.strftime("%d/%b_%H:%M")
     subprocess.call("git add .")
-    print "added"
     subprocess.call('git commit -m "new_data_"' + time_now)
-    print "committed"
     subprocess.call("git push origin master")
-    print "updated"
+    subprocess.call("mv new.txt test_on_" + time_now)
 
-
-def strip_data(test_type, data):
-    data = data.split(test_type, 1)[1]
-    f = data[0:data.index("\n")]
-    file_new.write(f + "\n")
-    return data
 
 with open("data.txt", 'a') as file_new:
-    data = find_total_time()
+    data = compile_data()
     count = data.count("\n")
     while count > 0:
         if package in data:
