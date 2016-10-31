cd35fa54f09bf901ab4334a0858b820f17e34f6f
Darius <darius@flytrex.com>
2016-10-31 12:54:18 +0200

    new_data_31/Oct/12:54

--- time.py
+++ time.py
@@ -25,7 +25,7 @@ def compile_data():
            + filter_data(routes)
 
 
-def strip_data(test_type, data):
+def strip_data(test_type, data, file_new):
     info = data.split(test_type, 1)[1]
     i = info[0:info.index("\n")]
     file_new.write(i + "\n")
@@ -35,26 +35,28 @@ def strip_data(test_type, data):
 def push_to_git():
     time_now = time.strftime("%d/%b/%H:%M")
     time_title = time.strftime("%d%b%Hh%Mm")
+
+    with open("data.txt", 'a') as file_new:
+        data = compile_data()
+        count = data.count("\n")
+        file_new.write("new data from %s" % time_now)
+        while count > 0:
+            if package in data:
+                data = strip_data(package, data, file_new)
+            elif users in data:
+                data = strip_data(users, data, file_new)
+            elif dashboard in data:
+                data = strip_data(dashboard, data, file_new)
+            elif site in data:
+                data = strip_data(site, data, file_new)
+            elif routes in data:
+                data = strip_data(routes, data, file_new)
+            count -= 1
+
     subprocess.call("mv new.txt test_on_%s.txt" % time_title)
     subprocess.call("git add .")
     subprocess.call('git commit -m "new_data_"' + time_now)
     subprocess.call("git push origin master")
 
-
-with open("data.txt", 'a') as file_new:
-    data = compile_data()
-    count = data.count("\n")
-    while count > 0:
-        if package in data:
-            data = strip_data(package, data)
-        elif users in data:
-            data = strip_data(users, data)
-        elif dashboard in data:
-            data = strip_data(dashboard, data)
-        elif site in data:
-            data = strip_data(site, data)
-        elif routes in data:
-            data = strip_data(routes, data)
-        count -= 1
 
 push_to_git()
