00e87936d75ca183a34a0f1a18e9bdbcaaff177e
Darius <darius@flytrex.com>
2016-11-10 11:17:43 +0200

    new_data_10/Nov/11/17

--- plot_graph.py
+++ plot_graph.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from pylab import plot, ylim, xlim, show, xlabel, ylabel, grid
+import numpy as np
 
 package_header = []
 add_mission_cancel = []
@@ -10,48 +12,42 @@ add_site_happy_flow = []
 site_header = []
 add_site_missing_param = []
 routes_header = []
+y = [add_site_cancel, add_site_happy_flow, add_site_missing_param, add_mission_missing_param, add_mission_cancel,
+     add_mission_happy_flow, user_header, package_header, site_header, routes_header]
+x = ["add_site_cancel", "add_site_happy_flow", "add_site_missing_param", "add_mission_missing_param",
+     "add_mission_cancel", "add_mission_happy_flow", "user_header", "package_header", "site_header", "routes_header"]
+ave = []
 
 
-def findAverage(test, name):
-    average = sum(test) / float(len(test))
+def find_average(test_time, name):
+    average = sum(test_time)/float(len(test_time))
+    ave.append(average)
     print name + ": %.2fs on average." % average
+
+
+def slot_data(line, i):
+    timing = float(line.split(":", 1)[1].strip("s\n"))
+    y[i].append(timing)
+
+
+def plot_graph():
+    a = np.arange(0, 37, 4)
+    plt.xticks(a, x)
+    plt.plot(a, y, "k.")
+    plt.plot(a, ave, "r-")
+    plt.show()
+    xlabel("test names")
+    ylabel("time taken(s)")
+    grid(True)
 
 
 with open("data.txt", 'r') as file:
     for line in file:
-        if "package_header" in line:
-            package_header.append(float(line.split(":", 1)[1].strip("s\n")))
-        elif "add_mission_cancel" in line:
-            add_mission_cancel.append(float(line.split(":", 1)[1].strip("s\n")))
-        elif "add_mission_happy_flow" in line:
-            add_mission_happy_flow.append(float(line.split(":", 1)[1].strip("s\n")))
-        elif "add_mission_missing_param" in line:
-            add_mission_missing_param.append(float(line.split(":", 1)[1].strip("s\n")))
-        elif "user_header" in line:
-            user_header.append(float(line.split(":", 1)[1].strip("s\n")))
-        elif "add_site_cancel" in line:
-            add_site_cancel.append(float(line.split(":", 1)[1].strip("s\n")))
-        elif "add_site_happy_flow" in line:
-            add_site_happy_flow.append(float(line.split(":", 1)[1].strip("s\n")))
-        elif "site_header" in line:
-            site_header.append(float(line.split(":", 1)[1].strip("s\n")))
-        elif "add_site_missing_param" in line:
-            add_site_missing_param.append(float(line.split(":", 1)[1].strip("s\n")))
-        elif "routes_header" in line:
-            routes_header.append(float(line.split(":", 1)[1].strip("s\n")))
-    findAverage(package_header, "package_header")
-    findAverage(add_site_happy_flow, "add_site_happy_flow")
-    findAverage(add_site_missing_param, "add_site_missing_param")
-    findAverage(add_site_cancel, "add_site_cancel")
+        for i in range(len(x)):
+            if x[i] in line:
+                slot_data(line, i)
 
-#
-#         if ":" in line:
-#             x.append(line.split(":", 1)[0])
-#             y.append(float(line.split(":", 1)[1].strip("s\n")))
-#     print x
-#     print y
-#
-# plt.plot(y)
-# plt.ylabel("time taken(s)")
-# plt.xlabel(x)
-# plt.show()
+for i in range(len(x)):
+    find_average(y[i], x[i])
+plot_graph()
+
